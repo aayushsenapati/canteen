97a0b6081799fcacb03a00f7cc219bf2b8fb7fa3
Aayush Senapati <aayushsenapati2002@gmail.com>
2023-11-06 22:34:33 +0530

    failed on windows

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,26 +1,17 @@
-<svelte:head>
-	<title>About</title>
-	<meta name="description" content="About this app" />
-</svelte:head>
-
-<div class="text-column">
-	<h1>About this app</h1>
-
-	<p>
-		This is a <a href="https://kit.svelte.dev">SvelteKit</a> app. You can make your own by typing the
-		following into your command line and following the prompts:
-	</p>
-
-	<pre>npm create svelte@latest</pre>
-
-	<p>
-		The page you're looking at is purely static HTML, with no client-side interactivity needed.
-		Because of that, we don't need to load any JavaScript. Try viewing the page's source, or opening
-		the devtools network panel and reloading.
-	</p>
-
-	<p>
-		The <a href="/sverdle">Sverdle</a> page illustrates SvelteKit's data loading and form handling. Try
-		using it with JavaScript disabled!
-	</p>
-</div>
+<main>
+    <h1>ABOUT</h1>
+    <hr />
+    <div>A website to find user profiles</div>
+</main>
+  
+  <style>
+    main {
+      font-size: 1.5rem;
+      margin: 4rem;
+      padding: 2rem;
+      color: gray;
+      justify-content: center;
+      box-shadow: 4px 5px 11px 10px lightgray;
+    }
+  </style>
+  
--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,10 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
 	server:{
-		port:3000
+		hmr: {
+			host: '0.0.0.0',
+			clientPort: 3001,
+		}
 	},
 	plugins: [sveltekit()]
 });
