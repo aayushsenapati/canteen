b18ac69e9c6bf4771f50075ca2a1a7129982750c
Aayush Senapati <aayushsenapati2002@gmail.com>
2023-11-13 17:11:59 +0000

    rest-update,res-add

--- canteen-manage/app/components/Card.js
+++ canteen-manage/app/components/Card.js
@@ -1,19 +1,27 @@
 "use client"
+import { useEffect } from 'react';
 
-const Card = ({ shop }) => {
+const Card = ({ shop ,onEdit}) => {
+  useEffect(() => {
+    console.log('Shop prop changed:', shop);
+  }, [shop]);
   return (
-    <div className="max-w-sm rounded overflow-hidden shadow-lg m-4">
+    <div className="flex flex-col bg-white rounded-lg shadow-md m-4 max-w-sm" onClick={()=>onEdit(shop)}>
       <div className="px-6 py-4">
-        <div className="font-bold text-xl mb-2">{shop.Name}</div>
+        <div className="font-bold text-xl mb-2 text-blue-600">Name: {shop.name}</div>
         <p className="text-gray-700 text-base">
-          {shop.Location}
+          <span className="font-semibold">Location:</span> {shop.location}
         </p>
         <p className="text-gray-700 text-base">
-          {shop.Status}
+          <span className="font-semibold">Status:</span> {shop.status}
         </p>
         <p className="text-gray-700 text-base">
-          {shop.EmailID}
+          <span className="font-semibold">Shop ID:</span> {shop.shop_id}
         </p>
+      </div>
+      <div className="px-6 pt-4 pb-2">
+        <span className="inline-block bg-blue-200 rounded-full px-3 py-1 text-sm font-semibold text-blue-700 mr-2 mb-2">#shop</span>
+        <span className="inline-block bg-blue-200 rounded-full px-3 py-1 text-sm font-semibold text-blue-700 mr-2 mb-2">#location</span>
       </div>
     </div>
   )
--- canteen-manage/app/shops/page.js
+++ canteen-manage/app/shops/page.js
@@ -3,13 +3,60 @@ import { useSession } from 'next-auth/react'
 import { useRouter } from 'next/navigation'
 import { useEffect, useState } from 'react'
 import Card from '../components/Card'
+import Modal from 'react-modal'
+Modal.setAppElement('body');
 
 const Dashboard = () => {
   const { data: session, status } = useSession()
   const router = useRouter()
   const [shops, setShops] = useState([])
   const [error, setError] = useState(null)
+  const [isModalOpen, setIsModalOpen] = useState(false)
+  const [isEditModalOpen, setIsEditModalOpen] = useState(false)
+  const [newShop, setNewShop] = useState({ Name: '', Location: '', Status: '' })
+  const [currentShop, setCurrentShop] = useState({ Name: '', Location: '', Status: '',Shop_ID:'' })
 
+  const handleInputChange = (event) => {
+    setNewShop({ ...newShop, [event.target.name]: event.target.value })
+  }
+
+  const handleUpdateChange = (event) => {
+    setCurrentShop({ ...currentShop, [event.target.name]: event.target.value })
+  }
+
+  const handleSubmitNew = async (event) => {
+    event.preventDefault()
+    const response = await fetch('/api/addRest', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ ...newShop, EmailID: session.user.email }),
+    })
+    const data = await response.json()
+    if (data.error) {
+      setError(data.error)
+    } else {
+      setShops([...shops, data])
+      setNewShop({ Name: '', Location: '', Status: '' })
+      setIsModalOpen(false)
+    }
+  }
+
+  const handleEditSubmit = async (event) => {
+    event.preventDefault();
+    const response = await fetch('/api/updateRest', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ ...currentShop, EmailID: session.user.email }),
+    });
+    const data = await response.json();
+    if (data.error) {
+      setError(data.error);
+    } else {
+      setShops(prevShops => prevShops.map(shop => shop.Shop_ID === currentShop.Shop_ID ? data : shop));
+      setCurrentShop({ Name: '', Location: '', Status: '',Shop_ID:'' });
+      setIsEditModalOpen(false);
+    }
+  };
   useEffect(() => {
     if (status === 'loading') return // Do nothing while loading
     if (!session) router.replace('/login') // If not authenticated, force log in
@@ -33,11 +80,127 @@ const Dashboard = () => {
 
   // If session exists, display content
   return (
-    <div className="flex flex-wrap justify-center">
-      {session && <p>Welcome, {session.user.email}</p>}
-      {error && <p>{error}</p>}
-      {shops.map(shop => <Card key={shop.Shop_ID} shop={shop} />)}
-    </div>
+    session && (
+      <div className="flex flex-col items-center justify-center mt-10">
+        <button 
+          onClick={() => setIsModalOpen(true)} 
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-10"
+        >
+          Add New Shop
+        </button>
+        <Modal 
+          isOpen={isModalOpen} 
+          onRequestClose={() => setIsModalOpen(false)} 
+          className="flex items-center justify-center mt-10 outline-none"
+        >
+          <div className="bg-white rounded-lg w-1/2 p-10 shadow-lg">
+            <form onSubmit={handleSubmitNew} className="space-y-4">
+              <label className="block">
+                <span className="text-gray-700">Name:</span>
+                <input 
+                  type="text" 
+                  name="Name" 
+                  value={newShop.Name} 
+                  onChange={handleInputChange} 
+                  required 
+                  className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                />
+              </label>
+              <label className="block">
+                <span className="text-gray-700">Location:</span>
+                <input 
+                  type="text" 
+                  name="Location" 
+                  value={newShop.Location} 
+                  onChange={handleInputChange} 
+                  required 
+                  className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                />
+              </label>
+              <label className="block">
+                <span className="text-gray-700">Status:</span>
+                <input 
+                  type="text" 
+                  name="Status" 
+                  value={newShop.Status} 
+                  onChange={handleInputChange} 
+                  required 
+                  className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                />
+              </label>
+              <button 
+                type="submit" 
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              >
+                Submit
+              </button>
+            </form>
+          </div>
+        </Modal>
+
+        <Modal 
+          isOpen={isEditModalOpen} 
+          onRequestClose={() => setIsEditModalOpen(false)} 
+          className="flex items-center justify-center mt-10 outline-none"
+        >
+          <div className="bg-white rounded-lg w-1/2 p-10 shadow-lg">
+            <form onSubmit={handleEditSubmit} className="space-y-4">
+              <label className="block">
+                <span className="text-gray-700">Name:</span>
+                <input 
+                  type="text" 
+                  name="Name" 
+                  value={currentShop.Name} 
+                  onChange={handleUpdateChange} 
+                  required 
+                  className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                />
+              </label>
+              <label className="block">
+                <span className="text-gray-700">Location:</span>
+                <input 
+                  type="text" 
+                  name="Location" 
+                  value={currentShop.Location} 
+                  onChange={handleUpdateChange} 
+                  required 
+                  className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                />
+              </label>
+              <label className="block">
+                <span className="text-gray-700">Status:</span>
+                <input 
+                  type="text" 
+                  name="Status" 
+                  value={currentShop.Status} 
+                  onChange={handleUpdateChange} 
+                  required 
+                  className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                />
+              </label>
+              <button 
+                type="submit" 
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              >
+                Submit
+              </button>
+            </form>
+          </div>
+        </Modal>
+
+        <div className="flex flex-wrap justify-center">
+          {shops.map(shop => <Card key={shop.shop_id} shop={shop} onEdit={(shop)=>{
+            setCurrentShop({
+              Name: shop.name,
+              Location: shop.location,
+              Status: shop.status,
+              Shop_ID: shop.shop_id
+            })
+            setIsEditModalOpen(true)
+          }} />)}
+        </div>
+      </div>
+    )
   )
 }
 
